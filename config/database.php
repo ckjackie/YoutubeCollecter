15863fa0c26a55ea7066c2617ecb5c8cb9b8c116
Jackie Wang <wch0212@gmail.com>
2022-01-05 23:19:52 +0800

    modify_database_url

--- config/database.php
+++ config/database.php
@@ -16,10 +16,10 @@ return [
     */
 
 
-   'default' => env('DB_CONNECTION', 'mysql'),
+   //'default' => env('DB_CONNECTION', 'mysql'),
    
    // Heroku
-   //'default' => env('postgres://aopojvmdyogngp:7e60f44961a635b2d4da3ad98c69aef2c4bfafe97ae7da35df1d1e80796ffb83@ec2-54-162-211-113.compute-1.amazonaws.com:5432/d9r03saekl029g', 'pgsql'),
+   'default' => env('postgres://aopojvmdyogngp:7e60f44961a635b2d4da3ad98c69aef2c4bfafe97ae7da35df1d1e80796ffb83@ec2-54-162-211-113.compute-1.amazonaws.com:5432/d9r03saekl029g', 'pgsql'),
 
 
     /*
